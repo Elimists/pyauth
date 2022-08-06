4b913b76c0fc8120be112a65f8b229434a06b573
Pran Pandey <pandey.pran@gmail.com>
2022-08-05 21:33:28 -0600

    Created Decorator package
    Decorator package will contain all decorators for api routes.
    Examples of  some decorators:
    @authorization_required
    @user_must_be_admin
    etc

--- Routes/auth_routes.py
+++ Routes/auth_routes.py
@@ -2,6 +2,9 @@ from flask import request, jsonify, make_response
 from Routes import Routes
 from Tools import GeneratorTools as gt, PasswordTools as pt, StringTools as st
 from Database import UserFactory, VerificationCodeFactory, SessionFactory, PasswordResetFactory
+from ErrorHandler import ErrorHandler as err
+from functools import wraps
+from Decorators import is_user_authorized
 
 CLIENT_DOMAIN = '127.0.0.1:5500'
 
@@ -9,87 +12,74 @@ CLIENT_DOMAIN = '127.0.0.1:5500'
 def register():
     
     jsonData = request.get_json()
-    
 
-    if 'email' not in jsonData or 'name' not in jsonData or 'password' not in jsonData: 
-        return jsonify({'error': True, 'message': 'Missing data. Not Acceptable'})
+    if not jsonData or 'email' not in jsonData or 'name' not in jsonData or 'password' not in jsonData:
+        return err.packet(True, 1000, "Inccorrect form submsision!")
 
-    if not jsonData['email']:
-        return jsonify({'error': True, 'message': 'Email field cannot be empty!'})
+    if not jsonData['email'] or not jsonData['name'] or not jsonData['password']:
+        return err.packet(True, 1001, "Missing required values. Not acceptable!")
     
     emailCheck = st(jsonData['email'])
     if not emailCheck.is_email_valid():
-        return jsonify({'error': True, 'message': 'Email is not valid!'})
-    
-    if not jsonData['name']:
-        return jsonify({'error': True, 'message': 'Name field cannot be empty!'})
+        return err.packet(True, 1002, "Email is not valid!")
 
     nameCheck = st(jsonData['name'])
     if not nameCheck.is_name_valid():
-        return jsonify({'error': True, 'message': 'That name is not valid!'})
+        return err.packet(True, 1003, "Name is not valid!")
 
-    if not jsonData['password']:
-        return jsonify({'error': True, 'message': 'Password is missing!'})   
-
-    passwordCheck = pt(jsonData['password'])
-    if not passwordCheck.is_password_strong():
-        return jsonify({'error': True, 'message': 'Password is weak!'})
+    passwordString = pt(jsonData['password'])
+    if not passwordString.is_password_strong():
+        return err.packet(True, 1004, "Password is not strong enough!")
 
     try:
         user = UserFactory()
         verification = VerificationCodeFactory()
     except:
-        return jsonify({'error': True, 'message': 'Can\'t connect to db.'})
+        return err.packet(True, 5000, "Couldn't initialize tables!")
 
     if user.userAlreadyExistsInDB(jsonData['email']):
-        return jsonify({'error': True, 'message': 'User already exists!'})
+        return err.packet(True, 5001, "User already exists! Please log in!")
     
-    hashedPassword = passwordCheck.encrypt_password()
+    hashedPassword = passwordString.encrypt_password()
     verificationCode = gt.verification_code_generator()
     try:
         user.createUser(jsonData['email'], jsonData['name'], hashedPassword)   
     except:
-        return jsonify({'error': True, 'message': 'User was NOT created!'})
+        return err.packet(True, 5000, "Couldn't initialize tables!")
 
     try:
         verification.saveVerificationCode(jsonData['email'], verificationCode)
     except:
-        return jsonify({'error': True, 'message': 'Verification code was NOT saved!'})
+        return err.packet(True, 5000, "Couldn't initialize tables!")
     
-    return jsonify({'error': False, 'message': 'user and verification code saved!'})
+    return err.packet(False, 2000, "Signed up successfull! Please check email for a verification code!")
 
 
 @Routes.route('/verify', methods=['POST'])
 def verify():
 
     if not request.headers.get('Content-Type'):
-        return jsonify({'error': True, 'message': 'Forbidden. Invalid content-type'}), 403
+        return err.packet(True, 999, "Missing required headers")
 
     jsonData = request.get_json()
 
-    if not jsonData:
-        return jsonify({'error': True, 'message': 'Missing data. Not acceptable'})
-    
-    if 'verificationCode' not in jsonData or 'email' not in jsonData:
+    if not jsonData or 'verificationCode' not in jsonData or 'email' not in jsonData:
         return jsonify({'error': True, 'message': 'Missing data. Not acceptable!'})
     
-    if not jsonData['verificationCode']:
-        return jsonify({'error': True, 'message': 'Code field cannot be empty!'})
-
-    if not jsonData['email']:
-        return jsonify({'error': True, 'message': 'Email cannot be null!'})
+    if not jsonData['verificationCode'] or not jsonData['email']:
+        return jsonify({'error': True, 'message': 'Recieved empty fields! Not acceptable!'})
     
     try:
         verificationCodeFactory = VerificationCodeFactory()
         user = UserFactory()
     except:
-        return jsonify({'error': True, 'message': 'Can\'t reach database!'})
+        return jsonify({'error': True, 'message': 'Unable to initialize.'})
 
     verificationCodeInDb = verificationCodeFactory.getVerificationCode(jsonData['email'])
-    if len(verificationCodeInDb) == 0:
+    if not verificationCodeInDb:
         return jsonify({'error': True, 'message': 'Verification code for this user does not exist!'})
 
-    if verificationCodeInDb[0][0] != jsonData['verificationCode']:
+    if verificationCodeInDb['verificationCode'] != jsonData['verificationCode']:
         return jsonify({'error': True, 'message': 'Code does not match!'})
     
     try:
@@ -110,6 +100,7 @@ def login():
     
     if not request.headers.get('Content-Type'):
         return jsonify({'error': True, 'message': 'Forbidden. Invalid content-type'}), 403
+    
     
     jsonData = request.get_json()
 
@@ -268,8 +259,11 @@ def delete_user():
 
     passwordFromDB = user.getUserPassword(jsonData['email'])[0][0]
     passwordHandler = pt(jsonData['password'].encode("utf-8"))
-    if not passwordHandler.check_password(passwordFromDB.encode("utf-8")):
-        return jsonify({'error': True, 'message': 'Iccorrect password. Could not delete user!'})
+    try:
+        if not passwordHandler.check_password(passwordFromDB.encode("utf-8")):
+            return jsonify({'error': True, 'message': 'Iccorrect password. Could not delete user!'})
+    except:
+        return jsonify({'error': True, 'message': 'Unable to delete user. Try resetting password and then delete your account!'})
     
     try:
         user.deleteUser(jsonData['email'])
@@ -318,51 +312,10 @@ def logout():
     return res
     
 
-@Routes.route('/is-user-authorized', methods=['GET'])
-def is_user_authorized():
-
-    if not request.cookies.get('appSessionId'):
-        return jsonify({'error': True, 'message': 'Not Authorized! Please log in!'})
-
-    cookieSessionId = request.cookies.get('appSessionId')
-    try:
-        sessionFactory = SessionFactory()
-    except:
-        return jsonify({'error': True, 'message': 'Can\'t initialize session!'})
-    
-    # Check if session exists
-    if not sessionFactory.sessionIdExists(cookieSessionId):
-        return jsonify({'error': True, 'message': 'Session doesn\'t exist!'})
-
-    # Check if session has expired
-    if sessionFactory.sessionHasExpired(cookieSessionId):
-        sessionFactory.deleteSession(cookieSessionId)
-        res = make_response(jsonify({'error': True, 'message': 'Cookie has expired'}))
-        res.set_cookie(
-            key="appSessionId", 
-            value='',
-            expires=0,
-            secure=False,
-            httponly=True
-        )
-        return res
-
-    # Check if session is about to expire
-    if sessionFactory.isSessionAboutToExpire(cookieSessionId):
-        sessionFactory.addHoursToSesionExpiryTime(cookieSessionId, 1)
-        sessionData = sessionFactory.getSessionData(cookieSessionId)
-        res = make_response(jsonify({'error': False, 'message': 'User is authorized'}))
-        res.set_cookie(
-            key="appSessionId", 
-            value=sessionData['sessionId'],
-            expires=sessionData['expiresOn'],
-            secure=False,
-            httponly=True
-            )
-        return res
-    
-    res = make_response(jsonify({'error': False, 'message': 'User is authorized'}))
-    return res
+@Routes.route('/some-authorized-route', methods=['GET'])
+@is_user_authorized
+def some_authorized_route():
+    return make_response(jsonify({'error': False, 'message': 'Some Error'}))
 
 
 def getPublicIpAddressOfClient():
