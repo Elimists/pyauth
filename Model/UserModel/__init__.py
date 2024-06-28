922b4355116a494d9048841b5e42f73c92ce1f85
Pran Pandey <pandey.pran@gmail.com>
2024-06-28 13:21:36 -0600

    Removed unsued models

--- Model/UserModel/__init__.py
+++ Model/UserModel/__init__.py
@@ -1,7 +1,5 @@
 from .User import User
 from .AdminRole import AdminRole
-from .UserRole import UserRole
 from .UserAddress import UserAddress
 from .UserDetails import UserDetails
-from .UserRoleMember import UserRoleMember
 from .AdminRoleMember import AdminRoleMember
